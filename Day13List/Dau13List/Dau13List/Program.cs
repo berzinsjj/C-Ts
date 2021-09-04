4299831b46f635d42d39cbfa2ec3133c9dc12a84
Janis <59865347+berzinsjj@users.noreply.github.com>
2021-09-04 13:32:16 +0300

    C

---
+++ Day13List/Dau13List/Dau13List/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dau13List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Piemers1();
+        }
+
+        static void Piemers1()
+        {
+            Console.WriteLine("Cik studenti?");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            List<string> cilvekuList = new List<string>();
+
+            for (int i = 0; i < input; i++)
+            {
+                Console.WriteLine("Ievadiet Vardu Uzvardu");
+
+                cilvekuList.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < cilvekuList.Count; i++)
+            {
+                Console.WriteLine("Students " + cilvekuList[i]);
+            }
+
+        }
+    }
+}
