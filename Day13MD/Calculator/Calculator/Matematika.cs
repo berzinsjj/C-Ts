8e4235d2c6e783d7cf9f1da79b5f66f6d150227e
Janis <59865347+berzinsjj@users.noreply.github.com>
2021-09-09 18:05:17 +0300

    C#ts

---
+++ Day13MD/Calculator/Calculator/Matematika.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Matematika
+    {
+        public static void MatematikaOut()
+        {
+            String izvele = "";
+
+            while (izvele != "Ne")
+            {
+                Console.WriteLine("Ievadi 1 skaitli");
+                Double skaitlis1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Izvelaties darbibu " + " + " + " - " + " * " + " / ");
+                string darbiba = Console.ReadLine();
+
+                Console.WriteLine("Ievadi 2 skaitli");
+                Double skaitlis2 = Convert.ToDouble(Console.ReadLine());
+
+                Double sum = 0;
+
+                if (darbiba == "+")
+                {
+                    sum = skaitlis1 + skaitlis2;
+                    Console.WriteLine("Jusu iznakums " + sum);
+                }
+                else if (darbiba == "-")
+                {
+                    sum = skaitlis1 - skaitlis2;
+                    Console.WriteLine("Jusu iznakums " + sum);
+                }
+                else if (darbiba == "*")
+                {
+                    sum = skaitlis1 * skaitlis2;
+                    Console.WriteLine("Jusu iznakums " + sum);
+                }
+                else if (darbiba == "/")
+                {
+                    sum = skaitlis1 / skaitlis2;
+                    Console.WriteLine("Jusu iznakums " + sum);
+                }
+
+                Console.WriteLine("Izmantot velreiz? Ja/Ne");
+                izvele = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Day13MD/Calculator/Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveiki. te kalkulators");
+
+            Matematika.MatematikaOut(); // KLASE NO CITA FAILA
+
+        }
+
+    }
+}
