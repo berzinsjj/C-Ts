33e55ec596619603ca8e8248954f81c89ac23740
Janis <59865347+berzinsjj@users.noreply.github.com>
2021-09-09 19:31:03 +0300

    C#

---
+++ Day_14/Day_14/Day_14/StudentList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14
+{
+    class StudentList
+    {
+         public static void List()
+        {
+            List<string> cilvekuList = new List<string>();
+
+            String choice = "";
+
+            while (choice != "0")
+            {
+                Console.WriteLine("0- Iziet");
+                Console.WriteLine("1- Pievienot");
+                Console.WriteLine("2- Izvadit");
+                
+                choice = Console.ReadLine();
+                if (choice == "0")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Ievadiet Studenta Vardu un Uzvardu");
+                string Students = Console.ReadLine();
+                
+
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine("Goodbye");
+                        break;
+                    case "1":
+                        cilvekuList.Add(Students);
+                        break;
+                    case "2":
+                        Console.WriteLine("Students " + cilvekuList);
+                        break;
+                    default:
+                        Console.WriteLine("Jaievada 0,1,2 , lai izveletos");
+                        break;
+
+
+                }
+
+            }
+        }
+    }
+}
