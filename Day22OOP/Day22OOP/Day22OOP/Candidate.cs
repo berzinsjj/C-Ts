85fcfbb89756babbdcb8696d61c260ac0aabcc40
Janis <59865347+berzinsjj@users.noreply.github.com>
2021-10-12 19:57:15 +0300

    c#

---
+++ Day22OOP/Day22OOP/Day22OOP/Candidate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22OOP
+{
+    class Candidate
+    {
+        public String name;
+        public String lastName;
+        public double hrAtzime;
+        public double tsAtzime;
+
+        public Candidate(String name, String lastName, double hrAtzime, double tsAtzime)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.hrAtzime = hrAtzime;
+            this.tsAtzime = tsAtzime;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(name + " " + lastName);
+            Console.WriteLine("HR-Atzime- " + hrAtzime );
+            Console.WriteLine("TSV-Atzime- " + tsAtzime);
+            Console.WriteLine("VIDEJA- " + (hrAtzime + tsAtzime)/2);
+        }
+    }
+}
---
+++ Day22OOP/Day22OOP/Day22OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day22OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Candidate c1 = new Candidate("Jānis", "Bērziņš", 8.7, 9.6);
+            c1.Print();
+
+            Console.WriteLine();
+
+            Candidate c2 = new Candidate("Juris", "Ābols", 6.6, 7.1);
+            c2.Print();
+        }
+    }
+}
