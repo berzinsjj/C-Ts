a30f58805c0af661dfd25302d895445429c0e752
Janis <59865347+berzinsjj@users.noreply.github.com>
2021-10-23 10:45:43 +0300

    c#

---
+++ Day24/DAy24_JSON/DAy24_JSON/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAy24_JSON
+{
+    class User
+    {
+        public String userName { get; set; }
+        public String passWord { get; set; }
+        public String name { get; set; }
+        public String lastName { get; set; }
+        public int year { get; set; }
+
+        public void InitStudent(String userName, String passWord, String name, String lastName, int year)
+        {
+            this.userName = userName;
+            this.passWord = passWord;
+            this.name = name;
+            this.lastName = lastName;
+            this.year = year;
+        }
+
+
+
+        public void Print()
+        {
+            Console.WriteLine("aaaaaa");
+            
+        }
+    }
+}
