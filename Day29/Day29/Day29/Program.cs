63dd296b2cf209152f3028ea31a964d4136efb22
Janis <59865347+berzinsjj@users.noreply.github.com>
2021-11-17 20:43:46 +0200

    c#

---
+++ Day29/Day29/Day29/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Piemers1();
+        }
+
+        static void Piemers1()
+        {
+            Console.WriteLine("Ievadiisi 5 vertibas mazais zikeri");
+           
+            List<string> cilvekuList = new List<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("zikerim jaievada vertiba");
+
+                cilvekuList.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < cilvekuList.Count; i++)
+            {
+                Console.WriteLine("Aug " + cilvekuList[i]);
+            }
+
+            for (int i = cilvekuList.Count; i--> 0;)
+            {
+                Console.WriteLine("Dilst " + cilvekuList[i]);
+            }
+
+        }
+    }
+}
